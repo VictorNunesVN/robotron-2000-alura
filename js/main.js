f6b27ccb7ad85d6f5ca21d1695500d0a489c3872
VictorNunesVN <58942853+VictorNunesVN@users.noreply.github.com>
2023-04-20 08:25:14 -0300

    atualização
    Colocando mais data-attributes no html e colocando objetos javaScript no .js

--- js/main.js
+++ js/main.js
@@ -11,19 +11,57 @@ function dizOi(nome){
 }
 dizOi('victor')*/
 
+/**
+ * Usando o data-attributes no html para desvincular o javaScript do css. no querySelectorAll, a informação 
+ * é buscada por "[]", diferente do "." para as classes
+ */
+const controle = document.querySelectorAll("[data-controle]")
+//variável pecas é um objeto que tem cinco itens 
+const pecas = {
+    "braco":{
+        "forca" : 29,
+        "poder" : 35,
+        "energia": -21,
+        "velocidade" : -5
+    },
+    "blindagem":{
+        "forca" : 41,
+        "poder" : 20,
+        "energia": 0,
+        "velocidade" : -20
+    },
+    "nucleos":{
+        "forca" : 0,
+        "poder" : 7,
+        "energia": 48,
+        "velocidade" : -4
+    },
+    "pernas":{
+        "forca" : 27,
+        "poder" : 21,
+        "energia": -32,
+        "velocidade" : 43
+    },
+    "foguetes":{
+        "forca" : 0,
+        "poder" : 28,
+        "energia": 0,
+        "velocidade" : -2
+    }
 
-const controle = document.querySelectorAll(".controle-ajuste")
-
+}
 controle.forEach((elemento)=>{
     elemento.addEventListener('click',(evento)=>{
         /*o evento.target.parentNode vai relacionar o clique com a div correspondente para que a soma e
         subtração de peças ocorre de forma correta para cada parte.*/
-        manipulaDados(evento.target.textContent, evento.target.parentNode)
+        /*Como tem o data-controle, não é mais necessário buscar o sinal de + e - no texo, e sim no data-attribute.Então o .textContent é substituido por .dataset.controle*/ 
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
     })
 })
 
 function manipulaDados(operacao,controle){
-    const peca = controle.querySelector(".controle-contador")
+    /*O uso do data-attribute também se aplica no contador.*/
+    const peca = controle.querySelector("[data-contador]")
     if (operacao === '-'){
         if (peca.value <= 0){
             alert('Já não há mais braços !!!')
